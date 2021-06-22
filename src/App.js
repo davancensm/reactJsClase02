10773f27dd95e94ed96c93c19385478b5e3b89bc
Mariano Davancens <mdavancens@exolgan.com>
2021-06-22 16:53:20 -0300

    Cuarto entregable.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import CardWithClass from './components/CardWithClass';
+
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
 
 function App() {
   return (
     <div className="App">
         <NavBar/>
         <ItemListContainer/>
-        <CardWithClass/>
+        <ItemCount stock={5} initial={1} onAdd={() => console.log('Articulo agregado')}/>
     </div>
   );
 }
--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,15 +1,41 @@
 import React, {useState} from 'react';
+import {IconButton,Card} from '@material-ui/core';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 
-function ItemCount(stock, onAdd, initial){
+
+function ItemCount({stock: initialStock, onAdd, initial}){
     const [count, setCount] = useState(initial);
+    const [stock, setStock] = useState(initialStock);
+
+    const addCount = () => {
+        if(stock >= 1 && count < initialStock){
+            setCount(count + 1)
+            setStock(stock - 1)
+        }
+    }
+    const subsCount = () => {
+        if(count > 0){
+            setCount(count - 1);
+            setStock(stock + 1);
+        }
+    }   
+    
 
     return(
-    <div>    
-        <button onClick={() => setCount(count + 1)}>+</button>
+    <Card>
+        <IconButton aria-label="Add" size="small">
+        <ArrowUpwardIcon onClick={addCount}>+</ArrowUpwardIcon>
+        </IconButton>
         {count}
-        <button onClick={() => setCount(count - 1)}>-</button>
-        <button onClick={onAdd}>Agregar al carrito.</button>
-    </div>);
+        <IconButton aria-label="Delete" size="small">
+        <ArrowDownwardIcon onClick={subsCount}>-</ArrowDownwardIcon>
+        </IconButton>
+        <IconButton color="primary" onClick={onAdd}aria-label="Agregar al carrito.">
+        <AddShoppingCartIcon />
+        </IconButton>
+    </Card>)
 }
 
 export default ItemCount;
